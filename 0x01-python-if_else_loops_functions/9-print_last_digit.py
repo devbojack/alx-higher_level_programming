c612dbe0de05128d6a2df61a8669c15691569447
Justus Lolwerikoi <dev.bojack@gmail.com>
2023-05-09 07:20:25 -0700

    9: Last digit printer

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,9 +1,9 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 def print_last_digit(number):
     if number >= 0:
         x = number % 10
     elif number < 0:
         x = (number * -1) % 10
 
-    print(f"{x}", end="")
+    print("{}".format(x), end="")
     return (x)
