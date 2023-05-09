d8432398eb5137ba4737bb858219751f9961f2b8
Justus Lolwerikoi <dev.bojack@gmail.com>
2023-05-09 01:07:29 -0700

    prints a string in uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+def uppercase(str):
+    upper_string = ""
+    for x in range(len(str)):
+        if ord(str[x]) > 96 and ord(str[x]) < 123:
+                upper_string += chr(ord(str[x]) - 32)
+                continue
+        upper_string += str[x]
+
+    print(f"{upper_string}")
+
