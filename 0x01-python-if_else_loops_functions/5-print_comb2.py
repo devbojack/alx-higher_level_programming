a44075c6f26cf483c6c34327bbaa424c768ce86b
Justus Lolwerikoi <dev.bojack@gmail.com>
2023-05-09 07:07:57 -0700

    5:00...99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for x in range(100):
     if x < 99:
-        print(f"{x:02d}, ", end="")
+        print("{:02d}, ".format(x), end="")
     else:
-        print(f"{x}")
+        print("{}".format(x))
