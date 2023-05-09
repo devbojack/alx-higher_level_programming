7df2b680bb74b324a4120966856afd3fc2b17af6
Justus Lolwerikoi <dev.bojack@gmail.com>
2023-05-09 01:45:26 -0700

    12: Fizz Buzz

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+def fizzbuzz():
+    for x in range(1, 101):
+        if (x % 3) == 0 or (x % 5) == 0:
+            if (x % 3) == 0 and (x % 5) == 0:
+                print("FizzBuzz", end=" ")
+            elif (x % 3) == 0:
+                print("Fizz", end=" ")
+            else:
+                print("Buzz", end=" ")
+        else:
+            print(f"{x} ", end=" ")
