c1743b3a1c7f3229e5cd0dab3c86558a2a50c96b
Justus Lolwerikoi <dev.bojack@gmail.com>
2023-05-11 02:14:28 -0700

    2: Dynamic script

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python3
 import sys
 
-"""
+if __name__ == '__main__':
+    """
 
-prints the number of and the list of its arguments
+    prints the number of and the list of its arguments
 
-"""
-val_arg = sys.argv
-arg_len = len(val_arg) - 1
+    """
+    val_arg = sys.argv
+    arg_len = len(val_arg) - 1
 
-if arg_len == 1:
-    print(arg_len, 'argument:')
-    for x in range(1, arg_len + 1):
-        print("{:d}: {}".format(x, val_arg[x]))
-if arg_len > 1:
-    print(arg_len, 'arguments:')
-    for x in range(1, arg_len + 1):
-        print("{:d}: {}".format(x, val_arg[x]))
-if arg_len == 0:
-    print(arg_len, 'arguments.')
+    if arg_len == 1:
+        print(arg_len, 'argument:')
+        for x in range(1, arg_len + 1):
+            print("{:d}: {}".format(x, val_arg[x]))
+    elif arg_len > 1:
+        print(arg_len, 'arguments:')
+        for x in range(1, arg_len + 1):
+            print("{:d}: {}".format(x, val_arg[x]))
+    elif arg_len == 0:
+        print(arg_len, 'arguments.')
