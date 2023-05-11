beb1c20f0ce95fe53384339c313e4980b768a884
Justus Lolwerikoi <dev.bojack@gmail.com>
2023-05-11 02:10:05 -0700

    1: add, sub, mul and div calc

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 from calculator_1 import add, sub, mul, div
 
-"""
+if __name__ == '__main__':
+    """
 
-Prints the calculation of add, sub,
-mul and div between two numbers
+    Prints the calculation of add, sub,
+    mul and div between two numbers
 
-"""
-a = 10
-b = 5
-print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
-print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
-print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
-print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
+    """
+    a = 10
+    b = 5
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+    print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
