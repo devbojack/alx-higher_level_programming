2927eaf6044ace27b9c689b912005600bca3ff11
Justus Lolwerikoi <dev.bojack@gmail.com>
2023-05-11 02:04:29 -0700

    0: 2 number calculator

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 from add_0 import add
-"""
 
-Prints calculations of 2 numbers 
+if __name__ == '__main__':
+    """
+    
+    Prints calculations of 2 numbers
 
-"""
-a = 1
-b = 2
-print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    """
+    a = 1
+    b = 2
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
