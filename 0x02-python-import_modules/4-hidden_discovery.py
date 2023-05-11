018aea1548e19e2bdc3c35af6f42548a68277fee
Justus Lolwerikoi <dev.bojack@gmail.com>
2023-05-11 02:38:32 -0700

    4: Hidden_4 names

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,16 +1,9 @@
 #!/usr/bin/python3
-
 import hidden_4
 
 if __name__ == '__main__':
-    """
-
-    prints all the names defined by the
-    compiled module hidden_4.pyc
-
-    """
     the_names = dir(hidden_4)
 
     for x in range(len(the_names)):
         if the_names[x][:2] != '__':
-            print("{}".format(the_names[x]))
+            print(the_names[x])
