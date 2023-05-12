edc86adfc481249948d3d0933ae7394925b0905f
Justus Lolwerikoi <dev.bojack@gmail.com>
2023-05-12 03:33:49 -0700

    4: Replace in a copy

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def new_in_list(my_list, idx, element):
+    my_len = len(my_list)
+
+    if idx >= 0 and idx <= my_len:
+        new_list = my_list.copy()
+        new_list[idx] = element
+        return (new_list)
+    else:
+        return (my_list)
