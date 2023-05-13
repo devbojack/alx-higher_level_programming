37ca8105ce6d0953a26d80a52065d6a2d69a6844
Justus Lolwerikoi <dev.bojack@gmail.com>
2023-05-13 14:16:25 -0700

    1: Return element at idx

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,6 +2,6 @@
 
 def element_at(my_list, idx):
     if idx < 0 or idx > len(my_list):
-        return None
+        return (None)
     else:
-        return my_list[idx]
+        return (my_list[idx])
