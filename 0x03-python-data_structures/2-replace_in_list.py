ef0f50cc75d2e992b2373374abd2389930084e59
Justus Lolwerikoi <dev.bojack@gmail.com>
2023-05-13 14:28:23 -0700

    2: Replace element at idx

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 
 def replace_in_list(my_list, idx, element):
-    my_len = len(my_list)
-
-    if idx <= my_len and idx >= 0:
-        my_list[idx] = element
+    if (idx < 0) or (idx > len(my_list) - 1):
         return (my_list)
     else:
+        my_list[idx] = element
         return (my_list)
