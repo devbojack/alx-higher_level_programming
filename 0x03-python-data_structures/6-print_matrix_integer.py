64ad7ba891389065ea782ba1b9cb61e8975ed23b
Justus Lolwerikoi <dev.bojack@gmail.com>
2023-05-13 14:47:35 -0700

    6: Print matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -5,9 +5,7 @@ def print_matrix_integer(matrix=[[]]):
 
     for x in range(my_len):
         for y in range(len(matrix[x])):
-            if y < 2:
-                skip = " "
-            else:
-                skip = ""
-            print("{:d}".format(matrix[x][y]), end=skip)
-        print("")
+            if y != 0:
+                print(" ", end='')
+            print("{:d}".format(matrix[x][y]), end='')
+        print()
