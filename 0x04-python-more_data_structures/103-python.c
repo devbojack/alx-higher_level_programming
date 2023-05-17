a876535fa0d87a401079f724ac4eff133a947f77
Justus Lolwerikoi <dev.bojack@gmail.com>
2023-05-17 08:12:31 -0700

    103: Fix versioning

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,18 +1,19 @@
-#include "/usr/include/python3.4/bytesobject.h"
+#include "/usr/include/python3.8/Python.h"
 #include <stdio.h>
 
-void print_hexn(const char *str, int n)i
+void print_hexn(const char *str, int n)
 {
-	for (int i = 0; i < n - 1; ++i)
-	{
+	int i = 0;
+
+	for (; i < n - 1; ++i)
 		printf("%02x ", (unsigned char) str[i]);
-	}
+
 	printf("%02x", str[i]);
 }
 
 void print_python_bytes(PyObject *p)
 {
-	pyBytesObject *clone = (PyBytesObject *) p;
+	PyBytesObject *clone = (PyBytesObject *) p;
 	int calc_bytes, clone_size = 0;
 
 	printf("[.] bytes object info\n");
