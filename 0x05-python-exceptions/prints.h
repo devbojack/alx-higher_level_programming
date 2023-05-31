578efb7b4d0a031cb3079b9ba1650498a369ac72
Justus Lolwerikoi <dev.bojack@gmail.com>
2023-05-31 13:39:47 -0700

    CPython #2: PyFloatObject

---
+++ 0x05-python-exceptions/prints.h
@@ -0,0 +1,10 @@
+#ifndef _PRINTS__
+#define _PRINTS__
+
+#include <Python.h>
+
+void print_python_list(PyObject *p);
+void print_python_bytes(PyObject *p);
+void print_python_float(PyObject *p);
+
+#endif
