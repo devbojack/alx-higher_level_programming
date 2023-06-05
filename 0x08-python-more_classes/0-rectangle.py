5bb7d5834a7b4a4b6ed19f468eba55b470d9bf0f
Justus Lolwerikoi <dev.bojack@gmail.com>
2023-06-05 03:35:46 -0700

    Simple Rect

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Rectanlge class
+
+"""
+
+
+class Rectangle:
+    """
+    Empty Rectanlge class
+    """
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,3 @@
+0x08. Python - More Classes and Objects
+Python
+OOP
