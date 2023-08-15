815d0efbefe5817daba6a0c34a0c536ab0113b5c
Justus Lolwerikoi <dev.bojack@gmail.com>
2023-08-15 09:54:02 -0700

    create table first_name

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,4 +1,2 @@
 -- Create a table called first_table
-CREATE TABLE IF NOT EXISTS first_table(
-    id int,
-    name varchar(256)) IN mysql;
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
