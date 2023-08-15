6f9ba036c5b5b4b1110935fef731b827509c2242
Justus Lolwerikoi <dev.bojack@gmail.com>
2023-08-15 10:05:17 -0700

    show description of a table

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,2 +1,2 @@
--- prints the full description of the table first_table
-SHOW CREATE TABLE first_table IN mysql;
+-- prints the full description of first_table
+SHOW CREATE TABLE first_table FROM hbtn_0c_0;
