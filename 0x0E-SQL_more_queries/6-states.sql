6218543d07059d8c7a4814e332821174d8fbc21c
Justus Lolwerikoi <dev.bojack@gmail.com>
2023-08-16 08:05:49 -0700

    Auto increments id

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,7 @@
+-- creates the database hbtn_0d_usa and the table states
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
+    id INT UNIQUE NOT NULL AUTO_INCREMENT,
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id)
+);
