262b3d0bf62a1039ebbcb6e917e6ca69ae7e4edd
Justus Lolwerikoi <dev.bojack@gmail.com>
2023-08-16 05:35:53 -0700

    Create user all priviledges

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,6 @@
--- creates the MySQL server user user_0d_1.
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+-- a script that creates the MySQL server user user_0d_1.
+CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES 
+ON *.*
+TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
