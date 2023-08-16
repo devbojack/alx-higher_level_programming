9ab1651c1c0560b41dd93465abe0668f130fc2a5
Justus Lolwerikoi <dev.bojack@gmail.com>
2023-08-16 08:14:02 -0700

    Foreign key

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,6 +1,6 @@
 -- creates the database hbtn_0d_usa and the table cities in hbtn_0d_usa
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS cities(
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
     id INT UNIQUE NOT NULL AUTO_INCREMENT,
     state_id INT NOT NULL,
     name VARCHAR(256) NOT NULL,
