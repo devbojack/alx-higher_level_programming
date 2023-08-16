ad023898afad39042ba2a2ee47adcd9005404784
Justus Lolwerikoi <dev.bojack@gmail.com>
2023-08-16 05:00:12 -0700

    Show priviledges

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
--- lists all privileges for user_0d_1 and user_0d_2 in localhost
+-- lists all privileges for user_0d_1 in localhost
+-- list all privilege for user_0d_2 in localhost
 SHOW GRANTS FOR user_0d_1@localhost;
 SHOW GRANTS FOR user_0d_2@localhost;
