fb949317cedba2d47a8273e75a5b854eb085bf73
Justus Lolwerikoi <dev.bojack@gmail.com>
2023-09-20 12:45:39 -0700

    Cities by states

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-import sys
-import MySQLdb
 """
 lists all cities from the database hbtn_0e_4_usa
 """
 
+import sys
+import MySQLdb
 
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
