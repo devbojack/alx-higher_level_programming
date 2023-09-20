54543188ae994fd4cb04ac5ea5fb4d4b3f9a7fa9
Justus Lolwerikoi <dev.bojack@gmail.com>
2023-09-20 11:13:46 -0700

    1 2 hot fix

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -11,14 +11,13 @@ if __name__ == '__main__':
     Access to the database and get the states
     from the database.
     """
-
     db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
                          passwd=argv[2], db=argv[3])
 
     cur = db.cursor()
     cur.execute("SELECT * FROM states \
-                 WHERE name LIKE BINARY '{}' \
-                 ORDER BY states.id ASC".format(argv[4]))
+                 WHERE name LIKE BINARY 'N%' \
+                 ORDER BY states.id ASC")
     rows = cur.fetchall()
 
     for row in rows:
--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,18 +1,26 @@
 #!/usr/bin/python3
-import sys
-import MySQLdb
-
-""" takes in an argument and displays all values
-    in the states table of hbtn_0e_0_usa where
-    name matches the argument
+"""
+This script takes in an argument and displays all values in the states
+where `name` matches the argument from the database `hbtn_0e_0_usa`
 """
 
+import MySQLdb
+from sys import argv
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("""SELECT * FROM states
-                WHERE name LIKE BINARY '{}'
-                ORDER BY states.id ASC""".format(sys.argv[4]).strip("'"))
-    [print(state) for state in c.fetchall()]
+if __name__ == '__main__':
+    """
+    Access to the database and get the states
+    from the database.
+    """
 
+    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+                         passwd=argv[2], db=argv[3])
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states \
+                 WHERE name LIKE BINARY '{}' \
+                 ORDER BY states.id ASC".format(argv[4]))
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
