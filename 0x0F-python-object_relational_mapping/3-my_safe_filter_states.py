1aeb545b56377b1370dd00aea9e9c6b2c16c510c
Justus Lolwerikoi <dev.bojack@gmail.com>
2023-09-20 11:20:08 -0700

    sql injection

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-import sys
-import MySQLdb
-
 """
 displays all values in the states table of
 hbtn_0e_0_usa where name matches the argument
 """
+
+
+import MySQLdb
+from sys import argv
 
 
 if __name__ == "__main__":
