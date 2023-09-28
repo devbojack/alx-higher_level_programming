0c668ab2184bfc0a915474865d93d3cddbbda824
Justus Lolwerikoi <dev.bojack>
2023-09-28 06:22:44 -0700

    Curl methods

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Takes in a URL and displays all HTTP methods the server will accept
-curl -sI "$1" -X OPTION | grep "Allow:" | cut -d':' -f2 | sed 's/ //'
+curl -sI "$1" -X OPTIONS | grep "Allow:" | cut -d':' -f2 | sed 's/ //'
