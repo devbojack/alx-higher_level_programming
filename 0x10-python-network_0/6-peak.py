3098c30ae01af90fd11e43019486b1e033d99cbf
devbojack <dev.bojack@gmail.com>
2023-09-28 08:22:42 -0700

    Find a peak

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# finds a peak in a list of unsorted integers
+def find_peak(list_of_integers):
+    if not list_of_integers:
+        return None
+
+    left = 0
+    right = len(list_of_integers) - 1
+
+    while left < right:
+        mid= (left + right) // 2
+        if list_of_integers[mid] < list_of_integers[mid + 1]:
+            left = mid + 1
+        else:
+            right = mid
+
+    return list_of_integers[left]
