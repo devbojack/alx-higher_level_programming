d29e576f36e7969d3a5db1834a756bb7ad0cc29f
Justus Lolwerikoi <dev.bojack>
2023-09-28 05:24:55 -0700

    curl body size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# Takes in a URL
-# Sends a request to that URL
-# And displays the size of the body of the response
+# Takes in a URL ,sends a request to that URL, displays the size of the body of the response
 curl -sI "$1" | grep "Content-Length" | cut -d' ' -f2
