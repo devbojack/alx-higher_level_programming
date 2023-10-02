87d10066a826c8f278aaed4923b9813ce80376a3
Justus Lolwerikoi <dev.bojack@gmail.com>
2023-10-02 07:02:06 -0700

    Status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,7 +7,7 @@ from urllib.request import Request, urlopen
 
 
 if __name__ == "__main__":
-    req = Request('https://intranet.hbtn.io/status')
+    req = Request('https://alx-intranet.hbtn.io/status')
 
     with urlopen(req) as res:
         content = res.read()
