cdaf828b0332cf4c7083969f771b6dd9a083f6d2
Justus Lolwerikoi <dev.bojack@gmail.com>
2023-10-02 06:58:46 -0700

    Status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,12 +3,17 @@
 - fetches https://intranet.hbtn.io/status.
 - uses urlib package
 """
-from urllib import request
+from urllib.request import Request, urlopen
+
 
 if __name__ == "__main__":
-    with request.urlopen("https://intranet.hbtn.io/status") as response:
-        response = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(response)))
-        print("\t- content: {}".format(response))
-        print("\t- utf8 content: {}".format(response.decode(encoding='utf-8')))
+    req = Request('https://intranet.hbtn.io/status')
+
+    with urlopen(req) as res:
+        content = res.read()
+        utf8_content = content.decode('utf-8')
+
+        print('Body response:')
+        print('\t- type: {_type}'.format(_type=type(content)))
+        print('\t- content: {_content}'.format(_content=content))
+        print('\t- utf8 content: {_utf8_c}'.format(_utf8_c=utf8_content))
