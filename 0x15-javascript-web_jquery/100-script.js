325df10c1ac76bab20fb6675f3857bac5f5aee8c
Justus Lolwerikoi <dev.bojack@gmail.com>
2023-11-01 13:32:20 -0700

    No jQuery - document loaded

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+	const headerElement = document.querySelector('header');
+
+	if (headerElement) {
+		headerElement.style.color = '#FF0000'
+	}
+});
