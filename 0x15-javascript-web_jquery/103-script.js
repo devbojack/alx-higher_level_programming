943c14ace90bc78ecd422459ad7a408cf44caba7
Justus Lolwerikoi <dev.bojack@gmail.com>
2023-11-01 13:43:30 -0700

    And press ENTER

---
+++ 0x15-javascript-web_jquery/103-script.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+	function fetchTranslation() {
+		var languageCode = $('#language_code').val();
+
+		$.get("https://www.fourtonfish.com/hellosalut/hello/", { lang: languageCode }, function (data) {
+			$('#hello').text(data.hello);
+		});
+	}
+
+	$('#btn_translate').click(fetchTranslation);
+
+	$('#language_code').keypress(function (event) {
+		if (event.which == 13) {
+			fetchTranslation();
+		}
+	});
+}):
